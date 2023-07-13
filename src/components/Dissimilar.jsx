cf940164313697075ea5e82b413a37d206234bdc
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-07-12 23:19:52 -0700

    synonyms functions

--- src/components/Dissimilar.jsx
+++ src/components/Dissimilar.jsx
@@ -1,33 +1,16 @@
 
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react'
-import arrow from '../assets/down-arrow.svg'
+// import React, { useEffect, useRef, useState } from 'react'
 
-const syn = [{word: 'brother', synonyms:['one','two','three', 'four', 'five'], antonyms: 
-['one','two','three', 'four', 'five','six','seven']}
-]
-const Disimilar = () => {
-    const [isMore, setIsMore] = useState(true)
-    // const [antonyms ,setAntonyms] = useState([])
-    let [more, setMore] = useState(10)
-    var myRef = useRef(0)
-    useEffect(() => {
-      let getHeight = myRef.current.clientHeight    
-      if (getHeight > 64 && isMore) {
-        setMore(more - 1)
-      }  
-    },[more])
 
-  return (
-    <>
-        <div className="butn" ref={myRef}>
-            <span>Dissimilar:</span> {syn[0].antonyms.slice(0,more).map((idx, x) => <button key={x} >{idx}</button> )}
-            <button 
-            className='arrow'
-            onClick={() => {setMore(syn[0].antonyms.length); setIsMore(!isMore)}}><img src={arrow} alt="down-arrow" /></button>
-        </div>
-    </>
-  )
-}
+// const Disimilar = ({antonyms}) => {
 
-export default Disimilar
+//   return (
+//     <>
+//         <>
+//           <span className='diss'>Dissimilar:</span> {antonyms.map((idx, x) => <button key={x} >{idx}</button> )}
+//         </>
+//     </>
+//   )
+// }
+
+// export default Disimilar
--- src/components/Similar.jsx
+++ src/components/Similar.jsx
@@ -1,33 +1,57 @@
-import axios from 'axios';
 import React, { useRef, useState, useEffect } from 'react'
 import arrow from '../assets/down-arrow.svg'
 
 
 
-const Similar = ({antonyms}) => {
+const Similar = ({antonyms, synonyms}) => {
     const [isMore, setIsMore] = useState(true)
+    const [br, setBr] = useState(false)
+    const [dissimilar, setDissimilar] = useState(false)
+    const [addMore, setAddMore] = useState(false)
     let [more, setMore] = useState(10)
     var myRef = useRef(0)
-
-    const syn = [{word: 'brother', synonyms:['one','two','three', 'four', 'five','six','seven'], antonyms: 
-['one','two','three', 'four', 'five','six','seven']}
-]
   
     useEffect(() => {
-      let getHeight = myRef.current.clientHeight    
+      let getHeight = myRef.current.clientHeight ;
       if (getHeight > 64 && isMore) {
-        setMore(more - 1)
+        setAddMore(true);
+        setMore(more - 1);
       }  
     },[more])
+
+    function handleChange() {
+      if (isMore) {
+        setMore(antonyms.length+synonyms.length)
+        setIsMore(!isMore)
+        setBr(true)
+        setDissimilar(true)
+      } 
+    }
 
   return (
     <>
         <div className="butn" ref={myRef}>
-            <span>Similar:</span> {syn[0].synonyms.slice(0,more).map((idx, x) => <button key={x} >{idx}</button> )}
-            <button 
-            className='arrow'
-            onClick={() => {setMore(syn[0].synonyms.length); setIsMore(!isMore)}}><img src={arrow} alt="down-arrow" 
-            /></button>
+            <>
+              {synonyms.length > 0 && antonyms.length > 0 ? <>
+                <span>Similar:</span> {synonyms.slice(0,more).map((idx, x) => <button key={x} >{idx}</button> )} {br&& <br /> }
+                {dissimilar && <><span className='diss'>Dissimilar:</span> {antonyms.slice(0,more).map((idx, x) => <button key={x} >{idx}</button> )}</>}  
+              </> : synonyms.length > 0 && antonyms.length == 0? <>
+                <span>Similar:</span> {synonyms.slice(0,more).map((idx, x) => <button key={x} >{idx}</button> )}
+              </>  : synonyms.length == 0 && antonyms.length > 0? <>
+                <span className='diss'>Dissimilar:</span> {antonyms.slice(0,more).map((idx, x) => <button key={x} >{idx}</button> )}
+              </>: ''
+              }
+            </>
+            {
+              addMore && <button 
+              className='arrow'
+              onClick={handleChange}>
+                <img src={arrow} alt="down-arrow" 
+                  style={{
+                    transform: !isMore? 'rotateX(180deg)': ''
+                  }} />
+            </button>
+            }
         </div>
     </>
   )

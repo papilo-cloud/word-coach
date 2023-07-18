da07c1601bbb115a47e520148292743118bddfe6
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-07-18 11:07:48 -0700

    some little changes

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -21,6 +21,8 @@ const Game = ({data}) => {
   const [correct1, setCorrect1] = useState(false);
   const [clicked, setClicked] = useState(false)
   const [clicked1, setClicked1] = useState(false)
+  const [click, setClick] = useState(false)
+  const [score, setScore] = useState(0)
   // console.log(synonyms)
  
   const setSyn = async() => {
@@ -40,43 +42,43 @@ const Game = ({data}) => {
   // const randomAntonyms = Math.floor(Math.random()* (antonyms.length -1))
   // const randomSynonyms = Math.floor(Math.random()* (synonyms.length - 1))
 
-  function handleClickOne(params,e) {
+  function handleClickOne(params) {
     if (index >= 4) {
       setIndex(0)
     } else{
       setIndex(index + 1)
     }
     setClicked(true)
+    setClick(true)
     setTimeout(() => {
       setClicked(false)
     }, 1000);
     if (guess[0].includes(params) ) {
       setCorrect(true)
-      e.stopPropagation()
+      setScore(score + 200)
     }
   else{
     setCorrect(false)
-    e.stopPropagation()
   }
 
   }
-  function handleClickTwo(params,e) {
+  function handleClickTwo(params) {
     if (index >= 4) {
       setIndex(0)
     } else{
       setIndex(index + 1)
     }
+    setClick(true)
     setClicked1(true)
     setTimeout(() => {
       setClicked1(false)
     }, 1000);
     if (guess[0].includes(params) ) {
       setCorrect1(true)
-      e.stopPropagation()
+      setScore(score + 200)
     }
   else{
     setCorrect1(false)
-    e.stopPropagation()
   }
   }
 
@@ -93,9 +95,14 @@ const Game = ({data}) => {
     <div className='main'>
         <div className="top">
             <p>word coach</p>
-            <div className="more">
+            {
+              click ? <div className="score">
+                <p>score</p><span></span>{score}
+              </div>:
+              <div className="more">
               <p>Learn new words</p>
             </div>
+            }
         </div>
         <div className="words">
             <p>which word is {guess[0].includes('opposite')? `the ${guess[0]} of `: `${guess[0]} to `}  {rand[index]}?</p>
